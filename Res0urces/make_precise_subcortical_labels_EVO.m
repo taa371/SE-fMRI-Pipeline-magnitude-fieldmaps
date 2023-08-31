1e4d45be21a83555bbae9ba0cf4767acdfeff937
Holland Reece Brown <86682821+holland-reece@users.noreply.github.com>
2023-08-31 17:13:16 -0400

    debug: another path broken by '//'

--- Res0urces/make_precise_subcortical_labels_EVO.m
+++ Res0urces/make_precise_subcortical_labels_EVO.m
@@ -1,5 +1,5 @@
 
-Labels = [8 47 26 58 18 54 11 50 17 53 13 52 12 51 10 49 16 28 60]; % these are the default subcortical labels;
+Labels = [8 47 26 58 18 54 11 50 17 53 13 52 12 51 10 49 16 28 60]; % these are the default subcortical labels
 tmp_dir = [Subdir '/func/rois/tmp']; % define the roi directory
 system(['mkdir -p ' tmp_dir]); % make the temporary dir. 
 cd(tmp_dir); % change dir. 
@@ -15,15 +15,14 @@ system(['fslmaths ' tmp_dir '/Label' num2str(i) '_Interp_Thresh_Bin.nii.gz -mul 
 
 end
 
-% merge the files;
+% merge the files
 system(['fslmerge -t ' tmp_dir '/FinalLabels.nii.gz ' tmp_dir '/Label*_Final.nii.gz']);
 system(['fslmaths ' tmp_dir '/FinalLabels.nii.gz -Tmax ' tmp_dir '/FinalLabels.nii.gz']);
 system(['wb_command -volume-label-import ' tmp_dir '/FinalLabels.nii.gz ' SubcorticalLabels ' ' Subdir '/func/rois/Subcortical_ROIs_acpc.nii.gz -discard-others']);
 system(['rm -rf ' tmp_dir]);
 
-% MNInonlinear;
-
-tmp_dir = [Subdir '/func/rois/tmp/']; % define the roi directory
+% MNInonlinear
+tmp_dir = [Subdir '/func/rois/tmp']; % define the roi directory
 system(['mkdir -p ' tmp_dir]); % make the temporary dir. 
 cd(tmp_dir); % change dir. 
 
@@ -38,7 +37,7 @@ system(['fslmaths ' tmp_dir '/Label' num2str(i) '_Interp_Thresh_Bin.nii.gz -mul 
 
 end
 
-% merge the files;
+% merge the files
 system(['fslmerge -t ' tmp_dir '/FinalLabels.nii.gz ' tmp_dir '/Label*_Final.nii.gz']);
 system(['fslmaths ' tmp_dir '/FinalLabels.nii.gz -Tmax ' tmp_dir '/FinalLabels.nii.gz']);
 system(['wb_command -volume-label-import ' tmp_dir '/FinalLabels.nii.gz ' SubcorticalLabels ' ' Subdir '/func/rois/Subcortical_ROIs_nonlin.nii.gz -discard-others']);
