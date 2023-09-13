2db88360247118755745519633f024f9f70ea8c2
Holland Reece Brown <86682821+holland-reece@users.noreply.github.com>
2023-09-13 15:21:13 -0400

    debug: missing '/' in paths in ICA-AROMA call

--- MultiEchofMRI-Pipeline/ICAAROMA_SE_denoise_EVO.sh
+++ MultiEchofMRI-Pipeline/ICAAROMA_SE_denoise_EVO.sh
@@ -48,24 +48,24 @@ AllScans=$(cat "$Subdir"/AllScans.txt) # NOTE: this is used for parallel process
 # rm "$Subdir"/AllScans.txt # remove intermediate file
 
 # func --------------------
-# for s in $AllScans ; do
+for s in $AllScans ; do
 
-# 	echo -e "$s"
+	echo -e "$s"
 
-# 	python2.7 /athena/victorialab/scratch/hob4003/ME_Pipeline/ICA-AROMA-master/ICA_AROMA.py -in "$Subdir"/func/rest"$s"/Rest_E1_acpc.nii.gz -out "$Subdir"/func/rest"$s"/Rest_ICAAROMA.nii.gz -m "$Subdir"/func/xfms/rest/T1w_nonlin_brain_func_mask.nii.gz -mc "$Subdir"/func/rest"$s"/MCF.par -dim 30 -den 'aggr'
+	python2.7 /athena/victorialab/scratch/hob4003/ME_Pipeline/ICA-AROMA-master/ICA_AROMA.py -in "$Subdir"/func/rest/"$s"/Rest_E1_acpc.nii.gz -out "$Subdir"/func/rest/"$s"/Rest_ICAAROMA.nii.gz -m "$Subdir"/func/xfms/rest/T1w_nonlin_brain_func_mask.nii.gz -mc "$Subdir"/func/rest/"$s"/MCF.par -dim 30 -den 'aggr'
 
-# # TEST
-# #python2.7 /athena/victorialab/scratch/hob4003/ME_Pipeline/ICA-AROMA-master/ICA_AROMA.py -in /athena/victorialab/scratch/hob4003/study_EVO/NKI_MRI_data/97043/func/rest/session_1/run_1/Rest_E1_acpc.nii.gz -out /athena/victorialab/scratch/hob4003/study_EVO/NKI_MRI_data/97043/func/rest/session_1/run_1/Rest_E1_AROMA.nii.gz -m /athena/victorialab/scratch/hob4003/study_EVO/NKI_MRI_data/97043/func/xfms/rest/T1w_nonlin_brain_func_mask.nii.gz -mc /athena/victorialab/scratch/hob4003/study_EVO/NKI_MRI_data/97043/func/rest/session_1/run_1/MCF.par -dim 30 -den 'aggr'
+# TEST
+#python2.7 /athena/victorialab/scratch/hob4003/ME_Pipeline/ICA-AROMA-master/ICA_AROMA.py -in /athena/victorialab/scratch/hob4003/study_EVO/NKI_MRI_data/97043/func/rest/session_1/run_1/Rest_E1_acpc.nii.gz -out /athena/victorialab/scratch/hob4003/study_EVO/NKI_MRI_data/97043/func/rest/session_1/run_1/Rest_E1_AROMA.nii.gz -m /athena/victorialab/scratch/hob4003/study_EVO/NKI_MRI_data/97043/func/xfms/rest/T1w_nonlin_brain_func_mask.nii.gz -mc /athena/victorialab/scratch/hob4003/study_EVO/NKI_MRI_data/97043/func/rest/session_1/run_1/MCF.par -dim 30 -den 'aggr'
 
-# done 
+done 
 # end function ------------
 
 # TEST: see if I can get parallelize to work in my cluster env
-func () {
-	echo -e "CHECK:\n\tSubdir=$2\n\tAtlasTemplate=$4\n\tAllScans=$6\n\tAromaPyDir=$7" # check vbls were properly handed off to new env
+# func () {
+# 	echo -e "CHECK:\n\tSubdir=$2\n\tAtlasTemplate=$4\n\tAllScans=$6\n\tAromaPyDir=$7" # check vbls were properly handed off to new env
 
-	python2.7 "$7"/ICA_AROMA.py -in "$2"/func/rest/"$6"/Rest_E1_acpc.nii.gz -out "$2"/func/rest/"$6"/Rest_E1_AROMA.nii.gz -m "$2"/func/xfms/rest/T1w_nonlin_brain_func_mask.nii.gz -mc "$2"/func/rest/"$6"/MCF.par -dim 30 -den 'aggr'  
+# 	python2.7 "$7"/ICA_AROMA.py -in "$2"/func/rest/"$6"/Rest_E1_acpc.nii.gz -out "$2"/func/rest/"$6"/Rest_E1_AROMA.nii.gz -m "$2"/func/xfms/rest/T1w_nonlin_brain_func_mask.nii.gz -mc "$2"/func/rest/"$6"/MCF.par -dim 30 -den 'aggr'  
 
-}
-export -f func
-parallel --jobs $NTHREADS func ::: $MEDIR ::: $Subdir ::: $Subject ::: $AtlasTemplate ::: $DOF ::: $AllScans ::: $AromaPyDir
+# }
+# export -f func
+# parallel --jobs $NTHREADS func ::: $MEDIR ::: $Subdir ::: $Subject ::: $AtlasTemplate ::: $DOF ::: $AllScans ::: $AromaPyDir
