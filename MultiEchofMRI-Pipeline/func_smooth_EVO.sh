48dd9b1a89faf6bcf8412e845203632db29a2b69
Holland Reece Brown <86682821+holland-reece@users.noreply.github.com>
2023-09-15 10:34:49 -0400

    debug: missing param from KernelSize.txt

--- MultiEchofMRI-Pipeline/func_smooth_EVO.sh
+++ MultiEchofMRI-Pipeline/func_smooth_EVO.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Additional smoothing before projecting onto another surface (part of the SE denoising pipeline)
 # Charles Lynch, Hussain Bukhari, Holland Brown
-# Updated 2023-09-14
+# Updated 2023-09-15
 
 Subject=$1
 StudyFolder=$2
@@ -30,7 +30,7 @@ for s in $sessions; do
 			for k in $KernelSize; do
 
 				# smooth with geodesic (for surface data) and Euclidean (for volumetric data) Gaussian kernels
-				wb_command -cifti-smoothing "$Subdir"/func/rest/session_"$s"/run_"$r"/"$c".dtseries.nii "$k" COLUMN "$Subdir"/func/rest/session_"$s"/run_"$r"/"$c"_s"$k".dtseries.nii \
+				wb_command -cifti-smoothing "$Subdir"/func/rest/session_"$s"/run_"$r"/"$c".dtseries.nii "$k" "$k" COLUMN "$Subdir"/func/rest/session_"$s"/run_"$r"/"$c"_s"$k".dtseries.nii \
 				-left-surface "$Subdir"/anat/T1w/fsaverage_LR32k/"$Subject".L.midthickness.32k_fs_LR.surf.gii -right-surface "$Subdir"/anat/T1w/fsaverage_LR32k/"$Subject".R.midthickness.32k_fs_LR.surf.gii -merged-volume
 
 			done
@@ -47,7 +47,7 @@ rm -rf "$Subdir"/workspace/
 # calculate temporal mean and standard deviation; demean and normalize dense time-series 
 wb_command -cifti-reduce "$Subdir"/func/rest/session_"$s"/run_"$r"/"$c".dtseries.nii MEAN "$Subdir"/func/rest/session_"$s"/run_"$r"/MEAN.dscalar.nii
 wb_command -cifti-reduce "$Subdir"/func/rest/session_"$s"/run_"$r"/"$c".dtseries.nii STDEV "$Subdir"/func/rest/session_"$s"/run_"$r"/STDEV.dscalar.nii
-wb_command -cifti-math '(x - mean) / stdev' "$Subdir"/func/rest/session_"$s"/run_"$r"/"$c".dtseries.nii -fixnan 0 -var x "$Subdir"/func/rest/session_"$s"/run_"$r"/"$c".dtseries.nii \
+wb_command -cifti-math '((x-mean)/stdev)' "$Subdir"/func/rest/session_"$s"/run_"$r"/"$c".dtseries.nii -fixnan 0 -var x "$Subdir"/func/rest/session_"$s"/run_"$r"/"$c".dtseries.nii \
 -var mean "$Subdir"/func/rest/session_"$s"/run_"$r"/MEAN.dscalar.nii -select 1 1 -repeat -var stdev "$Subdir"/func/rest/session_"$s"/run_"$r"/STDEV.dscalar.nii -select 1 1 -repeat
 
 # remove some intermediate files
