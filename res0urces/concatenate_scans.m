3ec0a920e23222c5d8ce8f9c8f8b6081563ffa4a
Holland Reece Brown <86682821+holland-reece@users.noreply.github.com>
2023-09-19 15:40:30 -0400

    toubleshoot final cifti not passed to main script

--- res0urces/concatenate_scans.m
+++ res0urces/concatenate_scans.m
@@ -31,58 +31,22 @@ function [Output,SessionIdx,FD] = concatenate_scans(Subdir,File,Sessions)
             [fd] = calc_fd(rp,TR);
                  
             % load the cifti file (*.dtseries.nii)
-            c = ft_read_cifti_mod([Subdir '/func/rest/session_' num2str(Sessions(s)) '/run_' num2str(r) '/' File '.dtseries.nii']);
-            c.data = c.data - mean(c.data,2); % demean
+            Output = ft_read_cifti_mod([Subdir '/func/rest/session_' num2str(Sessions(s)) '/run_' num2str(r) '/' File '.dtseries.nii']);
+            Output.data = Output.data - mean(Output.data,2); % demean
             
             % log the data
-            ConcatenatedData = [ConcatenatedData c.data]; % concatenate the demeaned CIFTIs
-            SessionIdx = [SessionIdx ; ones(size(c.data,2),1) * s ones(size(c.data,2),1) * r]; %  session index
+            ConcatenatedData = [ConcatenatedData Output.data]; % concatenate the demeaned CIFTIs
+            SessionIdx = [SessionIdx ; ones(size(Output.data,2),1) * s ones(size(Output.data,2),1) * r]; %  session index
             FD = [FD ; fd]; %  save framewise displacement
+
+            % Output = c; % this is the output cifti
+            Output.data = ConcatenatedData;
      
         end
             
     end
     
-    Output = c; % this is the output cifti
-    Output.data = ConcatenatedData;
-    
-    end
-    
-    function [fd]=calc_fd(rp,tr)
-    
-    % Nyquist creates a frequency plot of a dynamical system model
-    nyq = (1/tr)/2;
-    
-    % create a tailored stop band filter
-    stopband = [0.2 (nyq-0.019)];
-    [B,A] = butter(10,stopband/nyq,'stop');
-    
-    % apply stopband filter 
-    for i = 1:size(rp,2)
-        rp(:,i) = filtfilt(B,A,rp(:,i));
-    end
-    
-    % calculate backward difference
-    n_trs = round(2.5 / tr);
-    
-    fd = rp; % preallocate
-    fd(1:n_trs,:) = 0; % by convention
-    
-    % sweep the columns
-    for i = 1:size(rp,2)
+    % Output = c; % this is the output cifti
+    % Output.data = ConcatenatedData;
 
-        for ii = (n_trs+1):size(fd,1)
-            fd(ii,i) = abs(rp(ii,i)-rp(ii-n_trs,i));
-        end
-
-    end
-    
-    fd_ang = fd(:,1:3); % convert rotation columns into angular displacement...
-    fd_ang = fd_ang / (2 * pi); % fraction of circle
-    fd_ang = fd_ang * 100 * pi; % multiplied by circumference
-    
-    fd(:,1:3) = []; % delete rotation columns
-    fd = [fd fd_ang]; % add back in as angular displacement
-    fd = sum(fd,2); % add 2 to every element in the framewise displacement matrix
-    
 end
