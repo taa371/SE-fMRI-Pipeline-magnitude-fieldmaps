76d01d5d1494c13b31c5dc8005ee51ad3b7821c5
Holland Brown <hrreagor.brown@gmail.com>
2023-11-30 17:19:21 -0500

    adjust for task fMRI params

--- EVO_MEP_supplemental_scripts/rorden_get_slice_times.m
+++ EVO_MEP_supplemental_scripts/rorden_get_slice_times.m
@@ -3,7 +3,7 @@
 % Updated: 2023-09-01
 % Source: https://neurostars.org/t/deriving-slice-timing-order-from-philips-par-rec-and-console-information/17688/2
 
-TRsec = 1.399999;
+TRsec = 2.4;
 nSlices = 144;
 TA = TRsec/nSlices; %assumes no temporal gap between volumes
 bidsSliceTiming=[0:TA:TRsec-TA]; %ascending
