1c787bc9be6938c0e5088d9ae4db6857c608b3ba
Holland Brown <hrreagor.brown@gmail.com>
2023-11-30 17:19:05 -0500

    debug: wrong path

--- EVO_MEP_supplemental_scripts/edit_jsons.sh
+++ EVO_MEP_supplemental_scripts/edit_jsons.sh
@@ -2,49 +2,55 @@
 # Holland Brown
 # Updated: 2023-11-30
 
+# Fix parameter names in UW JSONs to match what ME Pipeline expects
+
 # Note: need jq installed
 # Note: can get slice timing by running rorden_get_slice_times.m
 
-inputJson=/Volumes/LACIE-SHARE/EVO_MRI/organized/NKI/NKI_task_params.json
-SubjectListTxt=/Volumes/LACIE-SHARE/EVO_MRI/organized/NKI/subjectlist.txt
-SessionsTxt=/Volumes/LACIE-SHARE/EVO_MRI/organized/NKI/Sessions.txt
-RunsTxt=/Volumes/LACIE-SHARE/EVO_MRI/organized/NKI/Runs.txt
+inputJson=/media/holland/EVO_Estia/EVO_MRI/organized/UW_task_params.json
+SubjectListTxt=/media/holland/EVO_Estia/EVO_MRI/organized/subjectlist.txt
+SessionsTxt=/media/holland/EVO_Estia/EVO_MRI/organized/Sessions.txt
+RunsTxt=/media/holland/EVO_Estia/EVO_MRI/organized/Runs.txt
 
 for Subject in $(cat "$SubjectListTxt"); do
 
     for Session in $(cat "$SessionsTxt"); do
 
-        UnprocFuncDir=/Volumes/LACIE-SHARE/EVO_MRI/organized/NKI/"$Subject"/func/unprocessed/task/floop/session_"$Session"/run_"$Run"
-        origJson="$UnprocFuncDir"/"$Subject"_floop_S"$Session"_R1.json
+        for Run in $(cat "$RunsTxt"); do
 
-        # save params from existing files in bash variables
-        echoTime=$( jq -r '.EchoTime' "$inputJson" )
-        repTime=$( jq -r '.RepetitionTime' "$inputJson" )
-        phEncodeDir=$( jq -r '.PhaseEncodingDirection' "$inputJson" )
-        sliceTime=$( jq -r '.SliceTiming' "$inputJson" )
-        estimEcho=$( jq -r '.EstimatedEffectiveEchoSpacing' "$origJson" )
+            UnprocFuncDir=/media/holland/EVO_Estia/EVO_MRI/organized/UW/"$Subject"/func/unprocessed/task/adjective/session_"$Session"/run_"$Run"
+            origJson="$UnprocFuncDir"/"$Subject"_S"$Session"_R"$Run"_adjective.json
 
-        # add new params to subject's resting-state json
-        cp "$origJson" "$UnprocFuncDir"/temp.json
-        jq --argjson jq_var $echoTime '. += {"EchoTime": $jq_var}' "$UnprocFuncDir"/temp.json >"$origJson"
-        rm "$UnprocFuncDir"/temp.json
+            # save params from existing files in bash variables
+            echoTime=$( jq -r '.EchoTime' "$inputJson" )
+            repTime=$( jq -r '.RepetitionTime' "$inputJson" )
+            phEncodeDir=$( jq -r '.PhaseEncodingDirection' "$inputJson" )
+            sliceTime=$( jq -r '.SliceTiming' "$inputJson" )
+            estimEcho=$( jq -r '.EstimatedEffectiveEchoSpacing' "$origJson" )
 
-        cp "$origJson" "$UnprocFuncDir"/temp.json
-        jq --argjson jq_var $repTime '. += {"RepetitionTime": $jq_var}' "$UnprocFuncDir"/temp.json >"$origJson"
-        rm "$UnprocFuncDir"/temp.json
+            # add new params to subject's resting-state json
+            cp "$origJson" "$UnprocFuncDir"/temp.json
+            jq --argjson jq_var $echoTime '. += {"EchoTime": $jq_var}' "$UnprocFuncDir"/temp.json >"$origJson"
+            rm "$UnprocFuncDir"/temp.json
 
-        cp "$origJson" "$UnprocFuncDir"/temp.json
-        jq --arg jq_var "$phEncodeDir" '. += { "PhaseEncodingDirection": $jq_var }' "$UnprocFuncDir"/temp.json >"$origJson"
-        rm "$UnprocFuncDir"/temp.json
+            cp "$origJson" "$UnprocFuncDir"/temp.json
+            jq --argjson jq_var $repTime '. += {"RepetitionTime": $jq_var}' "$UnprocFuncDir"/temp.json >"$origJson"
+            rm "$UnprocFuncDir"/temp.json
 
-        cp "$origJson" "$UnprocFuncDir"/temp.json
-        jq --argjson jq_var "$estimEcho" '. += { "EffectiveEchoSpacing": $jq_var }' "$UnprocFuncDir"/temp.json >"$origJson"
-        rm "$UnprocFuncDir"/temp.json
+            cp "$origJson" "$UnprocFuncDir"/temp.json
+            jq --arg jq_var "$phEncodeDir" '. += { "PhaseEncodingDirection": $jq_var }' "$UnprocFuncDir"/temp.json >"$origJson"
+            rm "$UnprocFuncDir"/temp.json
 
-        cp "$origJson" "$UnprocFuncDir"/temp.json
-        jq --argjson jq_var "$sliceTime" '. += { "SliceTiming": $jq_var }' "$UnprocFuncDir"/temp.json >"$origJson"
-        rm "$UnprocFuncDir"/temp.json
+            cp "$origJson" "$UnprocFuncDir"/temp.json
+            jq --argjson jq_var "$estimEcho" '. += { "EffectiveEchoSpacing": $jq_var }' "$UnprocFuncDir"/temp.json >"$origJson"
+            rm "$UnprocFuncDir"/temp.json
+
+            cp "$origJson" "$UnprocFuncDir"/temp.json
+            jq --argjson jq_var "$sliceTime" '. += { "SliceTiming": $jq_var }' "$UnprocFuncDir"/temp.json >"$origJson"
+            rm "$UnprocFuncDir"/temp.json
     
+        done
+
     done
 
 done
