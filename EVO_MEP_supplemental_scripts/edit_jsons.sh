b6f9b49903e2f034a71393ce0b40f2f9b06e813d
Holland Reece Brown <86682821+holland-reece@users.noreply.github.com>
2023-12-01 10:26:41 -0500

    update comments; add description and ingredients

--- EVO_MEP_supplemental_scripts/edit_jsons.sh
+++ EVO_MEP_supplemental_scripts/edit_jsons.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 # Holland Brown
-# Updated: 2023-11-30
+# Updated: 2023-12-01
 
-# Fix parameter names in UW JSONs to match what ME Pipeline expects
+#                    >>>>    Edit parameter names in UW JSONs to match what ME Pipeline expects    <<<<
 
-# Note: need jq installed
-# Note: can get slice timing by running rorden_get_slice_times.m
+#                                               >>>>    Description    <<<<
+
+    # For EVO study, UW scans were collected on a Philips scanner and have different parameter names in their JSON files than JSONs from Siemens scans.
+    # The Liston/Power labs' Multi-Echo fMRI Preprocessing Pipeline depends on values extracted from JSONs in the Siemens format.
+    # This script takes parameter values that already exist in Philips JSONs (except slice times and phase encoding direction) and pastes them into the Philips JSONs under Siemens parameter names.
+    # It means those parameters will be listed twice in the JSONs, but the ME Pipeline will only read the parameters with Siemens labels.
+
+#                                               >>>>    Ingredients    <<<<
+
+    # (1) Need jq installed in bash environment (package for editing JSON files)
+
+    # (2) Reference JSON file with parameters and format you want -> 'inputJson'
+        # Copy and paste parameters you need from the Philips JSONs into 'inputJson'; change parameter names to match Siemens
+        # Siemens names of parameters MEP needs: EchoTime, RepetitionTime, PhaseEncodingDirection, EffectiveEchoSpacing, SliceTiming
+
+    # (3) SliceTiming (usually not in Philips JSONs already) -> you could exclude this and MEP will skip slice-time correction, but that's not ideal
+        # Can calculate slice timing by running rorden_get_slice_times.m; then copy and paste into 'inputJson'
+
+    # (4) PhaseEncodingDirection (usually not in Philips JSONs already) -> this parameter is very important; excluding it will break MEP
+        # NOTE: Philips JSONs have a parameter called "PhaseEncodingAxis"; this is not the same as PhaseEncodingDirection!
+        # Figure out PhaseEncodingDirection from +/- signs of x,y,z axes in header info of a Philips func image (use command 'fslhd'; must have fsl installed)
+        # Having an incorrect value here can be catastrophic, so make sure you have the correct PhaseEncodingDirection (it's 'j-' for EVO task and rest fMRI)
+
+
 
 inputJson=/Volumes/EVO_Estia/EVO_MRI/organized/UW_task_params.json
 SubjectListTxt=/Volumes/EVO_Estia/EVO_MRI/organized/subjectlist_tmp.txt
