a91aea7fa8e93c1856766518a9f0bb09828f27b8
Holland Reece Brown <86682821+holland-reece@users.noreply.github.com>
2023-12-05 15:18:19 -0500

    fix paths for floop outputs

--- MultiEchofMRI-Pipeline-EVOtask/resources/make_precise_subcortical_labels_EVOfloop.m
+++ MultiEchofMRI-Pipeline-EVOtask/resources/make_precise_subcortical_labels_EVOfloop.m
@@ -1,6 +1,6 @@
 
 Labels = [8 47 26 58 18 54 11 50 17 53 13 52 12 51 10 49 16 28 60]; % these are the default subcortical labels
-tmp_dir = [Subdir '/func/task/floop/rois/tmp']; % define the roi directory
+tmp_dir = [Subdir '/func/floop/rois/tmp']; % define the roi directory
 %system(['mkdir ' tmp_dir]); % make the temporary dir. 
 cd(tmp_dir); % change dir. 
 
@@ -18,11 +18,11 @@ end
 % merge the files
 system(['fslmerge -t ' tmp_dir '/FinalLabels.nii.gz ' tmp_dir '/Label*_Final.nii.gz']);
 system(['fslmaths ' tmp_dir '/FinalLabels.nii.gz -Tmax ' tmp_dir '/FinalLabels.nii.gz']);
-system(['wb_command -volume-label-import ' tmp_dir '/FinalLabels.nii.gz ' SubcorticalLabels ' ' Subdir '/func/task/floop/rois/Subcortical_ROIs_acpc.nii.gz -discard-others']);
+system(['wb_command -volume-label-import ' tmp_dir '/FinalLabels.nii.gz ' SubcorticalLabels ' ' Subdir '/func/floop/rois/Subcortical_ROIs_acpc.nii.gz -discard-others']);
 %system(['rm -rf ' tmp_dir]);
 
 % MNInonlinear
-tmp_dir = [Subdir 'func/task/floop/rois/tmp_nonlin']; % define the roi directory
+tmp_dir = [Subdir 'func/floop/rois/tmp_nonlin']; % define the roi directory
 %system(['mkdir ' tmp_dir]); % make the temporary dir. 
 cd(tmp_dir); % change dir. 
 
@@ -40,5 +40,5 @@ end
 % merge the files
 system(['fslmerge -t ' tmp_dir '/FinalLabels.nii.gz ' tmp_dir '/Label*_Final.nii.gz']);
 system(['fslmaths ' tmp_dir '/FinalLabels.nii.gz -Tmax ' tmp_dir '/FinalLabels.nii.gz']);
-system(['wb_command -volume-label-import ' tmp_dir '/FinalLabels.nii.gz ' SubcorticalLabels ' ' Subdir 'func/task/floop/rois/Subcortical_ROIs_nonlin.nii.gz -discard-others']);
+system(['wb_command -volume-label-import ' tmp_dir '/FinalLabels.nii.gz ' SubcorticalLabels ' ' Subdir 'func/floop/rois/Subcortical_ROIs_nonlin.nii.gz -discard-others']);
 % system(['rm -rf ' tmp_dir]);
