853b9c4edbb4a966ad69300f418aae7ea9fce998
Holland Reece Brown <86682821+holland-reece@users.noreply.github.com>
2023-12-11 12:18:59 -0500

    debug: fix subdir path

--- MultiEchofMRI-Pipeline-EVOtask/func_wrapper_EVOtask.sh
+++ MultiEchofMRI-Pipeline-EVOtask/func_wrapper_EVOtask.sh
@@ -47,7 +47,7 @@ if [ ! -d "$StudyFolder/$Subject/func/$TaskName" ]; then
 	mkdir "$StudyFolder"/"$Subject"/func/"$TaskName"
 fi
 if [ ! -d "$StudyFolder/$Subject/func/xfms/$TaskName" ]; then
-	mkdir "$StudyFolder"/"$Subject"/xfms/func/"$TaskName"
+	mkdir "$StudyFolder"/"$Subject"/func/xfms/"$TaskName"
 fi
 
 # (1) Process all field maps & create an average image for cases where scan-specific maps are unavailable
