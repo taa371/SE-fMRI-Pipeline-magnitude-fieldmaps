e8473c73cb7d8b2494df2fa738aab237d4533315
holland-reece <hrreagor.brown@gmail.com>
2024-05-24 16:39:55 -0400

    update comments

--- MultiEchofMRI-Pipeline-EVOtask/denoise_ICAAROMA_SE_EVOtask.sh
+++ MultiEchofMRI-Pipeline-EVOtask/denoise_ICAAROMA_SE_EVOtask.sh
@@ -38,7 +38,7 @@ for s in $sessions ; do
 	runs=$(seq 1 1 "${#runs[@]}")
 	for r in $runs ; do
 		echo "session_$s/run_$r" >> "$Subdir"/AllScans.txt  
-        echo 10 >> "$Subdir"/func/unprocessed/task/"$TaskName"/session_"$s"/run_"$r"/rmVols.txt
+        echo 4 >> "$Subdir"/func/unprocessed/task/"$TaskName"/session_"$s"/run_"$r"/rmVols.txt
 	done
 done
 
